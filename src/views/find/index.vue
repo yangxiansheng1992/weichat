3ff718c2ccf368a79bb7476dfe0633c5364dd7dd
thomas <yangthomas1992@gmail.com>
2019-05-30 19:01:43 +0800

    111

--- src/views/find/index.vue
+++ src/views/find/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="find">
-    我是发现页面
-    <router-view/>
+    我是发现页面,要有朋友圈哦
+    <router-view />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.find{
+.find {
   padding: 50px 0 0;
 }
 </style>
