2ceae0ee44eecbc11025175659d12e18fa52852f
thomas <yangthomas1992@gmail.com>
2019-05-30 19:10:46 +0800

    123

--- src/views/clientList/index.vue
+++ src/views/clientList/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="clientList">
-    我是客户列表
-    <router-view/>
+    我是客户列表，列表很长
+    <router-view />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.clientList{
+.clientList {
   padding: 50px 0 0;
 }
 </style>
